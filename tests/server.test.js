6b7561bba60fd48c0191fe091856781c1eedd8d6
sri85 <sripathi@roxtr.com>
2018-03-28 22:10:06 +0100

    added jasmine env to eslint

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,21 +1,20 @@
 const timeServer = require('../server');
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let should = chai.should();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.should();
 chai.use(chaiHttp);
-const {expect} = require('chai');
 
 
-describe("GET /", () => {
-    it("returns status code 200", (done) => {
-        chai.request(timeServer)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
-    });
+describe('GET /', () => {
+  it('returns status code 200', (done) => {
+    chai.request(timeServer)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
 });
-
 
