bcc3eed6ce1c6a847baed0cf29a79a46ff78fd8a
sri85 <sripathi@roxtr.com>
2018-04-08 00:25:48 +0100

    updated model

--- api/models/mongoClient.js
+++ api/models/mongoClient.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost:27017/urlShortener');
 
+
 //  Schema for URL Shortener
 
 mongoose.Promise = global.Promise;
@@ -14,6 +15,9 @@ const urlShortenerSchema = Schema({
   original: String,
   shortCode: { type: Number, index: true },
 });
+
+urlShortenerSchema.index({ shortCode: 1 });
+urlShortenerSchema.set('autoIndex', false);
 
 const URLShortenerModel = mongoose.model('URLShortener', urlShortenerSchema);
 
