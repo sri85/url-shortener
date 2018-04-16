80defd69415b7447132df3c63464bcc794c4f200
sri85 <sripathi@roxtr.com>
2018-04-16 21:37:59 +0100

    first set of mongo tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,20 +1,29 @@
-// const timeServer = require('../server');
-//
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-//
-// chai.should();
-// chai.use(chaiHttp);
-//
-//
-// describe('GET /', () => {
-//   it('returns status code 200', (done) => {
-//     chai.request(timeServer)
-//       .get('/')
-//       .end((err, res) => {
-//         res.should.have.status(200);
-//         done();
-//       });
-//   });
-// });
+const urlShortener = require('../server');
 
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.should();
+chai.use(chaiHttp);
+
+
+describe('GET /', () => {
+  it('API returns status code 404 when we pass an invalid route', (done) => {
+    chai.request(urlShortener)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+  it('API returns status code 404 when we pass an invalid route', (done) => {
+    chai.request(urlShortener)
+      .get('/api/new/http://example.com')
+      .end((err, res) => {
+        res.should.have.status(200);
+        // eslint-disable-next-line no-unused-expressions
+        expect(res).to.be.json;
+        done();
+      });
+  });
+});
